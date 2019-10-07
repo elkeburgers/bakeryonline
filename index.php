d1fb16a2075fb681d24b02821445455946631131
Elke Burgers <elke.burgers@hotmail.com>
2019-10-07 11:00:20 -0300

    Criaçao de arquivo header e uso de função include.

--- index.php
+++ index.php
@@ -22,38 +22,8 @@
     <title>Bakery Online</title>
 </head>
 <body>
-    <header class="navbar">
-        <h1 id=logo>
-            <?php echo $nomeSistema; ?>
-        </h1>
-        <nav>
-            <ul class="nav">
-                <?php if(isset($usuario) && $usuario != []) {?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cursos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Olá <?php echo $usuario["nome"]; ?></a>
-                    </li>
-                <?php } else { ?>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cadastro</a>
-                    </li>
-                <?php } ?>   
-            </ul>
-        </nav>
-    </header>
-    <nav class="nav-bar bg-dark">
-        <ul class="nav">
-            <?php foreach($categorias as $categoria){?>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><?php echo $categoria; ?></a>
-                </li>
-            <?php } ?>
-    </nav>    
+<!-- colocamos o header em outro arquivo para não ter que copiar o código todas as páginas, apenas chama o arquivo cada vez que utilizá-lo. -->
+    <?php include_once("header.php") ?>   
     <main>
         <section class="container mt-4">
             <div class="row justify-content-around">
