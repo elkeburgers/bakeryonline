63c3822f40f793e49e5b4ffc70697b061860ae16
Elke Burgers <elke.burgers@hotmail.com>
2019-10-16 08:38:59 -0300

    teste de extensão beatify

--- header.php
+++ header.php
@@ -1,34 +1,34 @@
 <?php include_once("variaveis.php") ?>
 
 <header class="navbar">
-        <h1 id=logo>
-            <?php echo $nomeSistema; ?>
-        </h1>
-        <nav>
-            <ul class="nav">
-                <?php if(isset($usuario) && $usuario != []) {?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cursos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Olá <?php echo $usuario["nome"]; ?></a>
-                    </li>
-                <?php } else { ?>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cadastro</a>
-                    </li>
-                <?php } ?>   
-            </ul>
-        </nav>
-    </header>
-    <nav class="nav-bar bg-dark">
+    <h1 id=logo>
+        <?php echo $nomeSistema; ?>
+    </h1>
+    <nav>
         <ul class="nav">
-            <?php foreach($categorias as $categoria){?>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><?php echo $categoria; ?></a>
-                </li>
+            <?php if(isset($usuario) && $usuario != []) {?>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Cursos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Olá <?php echo $usuario["nome"]; ?></a>
+            </li>
+            <?php } else { ?>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Cadastro</a>
+            </li>
             <?php } ?>
-    </nav> 
+        </ul>
+    </nav>
+</header>
+<nav class="nav-bar bg-dark">
+    <ul class="nav">
+        <?php foreach($categorias as $categoria){?>
+        <li class="nav-item">
+            <a class="nav-link text-white" href="#"><?php echo $categoria; ?></a>
+        </li>
+        <?php } ?>
+</nav>
